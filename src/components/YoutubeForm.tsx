57618630a8b768ade1b9ae6d8671e52c519acf35
PrudviRajasekhar <rajap.lotus@gmail.com>
2023-04-25 22:13:01 +0400

    first commit

---
+++ src/components/YoutubeForm.tsx
@@ -0,0 +1,94 @@
+import { useForm } from "react-hook-form";
+import { DevTool } from "@hookform/devtools";
+import { type } from "os";
+
+type FormValues = {
+  username: string;
+  email: String;
+  channel: String;
+};
+
+export const YoutubeForm = () => {
+  const form = useForm<FormValues>({
+    defaultValues: {
+      username: "abc",
+      email: "",
+      channel: "",
+    },
+  });
+  const { register, control, handleSubmit, formState } = form;
+  const { errors } = formState;
+
+  const onSubmit = (data: FormValues) => {
+    console.log("form submitted", data);
+  };
+
+  return (
+    <div>
+      <h1>YouTube Form</h1>
+
+      <form onSubmit={handleSubmit(onSubmit)} noValidate>
+        <div>
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            id="username"
+            {...register("username", {
+              required: {
+                value: true,
+                message: "Username is required",
+              },
+            })}
+          />
+          <p className="error">{errors.username?.message}</p>
+        </div>
+
+        <div>
+          <label htmlFor="email">E-mail</label>
+          <input
+            type="email"
+            id="email"
+            {...register("email", {
+              required: "email is mandatory",
+              pattern: {
+                value:
+                  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+                message: "Invalid email format",
+              },
+              validate: {
+                notAdmin: (fieldValue) => {
+                  return (
+                    fieldValue !== "rajap.lotus@gmail.com" ||
+                    "Enter different email address"
+                  );
+                },
+                notBlackListed: (fieldValue) => {
+                  return (
+                    !fieldValue.endsWith("baddomain.com") ||
+                    "This domain is not supported"
+                  );
+                },
+              },
+            })}
+          />
+          <p className="error">{errors.email?.message}</p>
+        </div>
+
+        <div>
+          <label htmlFor="channel">Channel</label>
+          <input
+            type="text"
+            id="channel"
+            {...register("channel", {
+              required: "Channel name is mandatory",
+            })}
+          />
+          <p className="error">{errors.channel?.message}</p>
+        </div>
+
+        <button>Submit</button>
+      </form>
+      <DevTool control={control} />
+    </div>
+  );
+};
